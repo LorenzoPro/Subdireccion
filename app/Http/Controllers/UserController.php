34dc8fa057e0f4346524a5ef4d0aa8fbee8c6038
Unknown <Lorenzomagic21@gmail.com>
2018-09-20 18:25:52 -0600

    Controlador de usuarios en proceso

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator;
+use App\User;
 
 class UserController extends Controller
 {
-    //
+  //Checar si esta logeado
+  public function __construct()
+  {
+      $this->middleware('auth');
+  }
+  public function index(){
+    //Consulta directamente a la tabla
+      $usuarios=\DB::table('users')
+        //->where('id','=','1')
+        //->take(10);
+        ->orderBy('id','name')
+        ->get();
+      $titulo = "ITSNCG - Usuarios";
+      return view('admin.usuarios')
+        ->with('titulo', $titulo)
+        ->with('usuarios', $usuarios);
+  }
 }
